56984601ab7bdcd99bba17d1526fc21b43b2aed9
MORGANJASON <420morganjason@gmail.com>
2023-06-19 13:43:30 +0300

    completed

--- server/app.py
+++ server/app.py
@@ -3,3 +3,21 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+
+# @app.route registers the index()
+# function with the Flask application instance app
+# its an instance method that modifies app, creating a rule that requests
+# for the base URL (/) should show our index: a page with a header that says "Welcome to my app!"
+@app.route("/")
+def index():
+    return "<h1>Welcome to my app!</h1>"
+
+
+@app.route("/<username>")
+def user(username):
+    return f"<h1>Profile for {username}</h1>"
+
+
+if __name__ == "__main__":
+    app.run(port=5555)
